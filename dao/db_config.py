ea3f6370af4668b162bcc685b1e12f77de83f761
Antonio José <antonioalves073d@gamial.com>
2025-10-26 21:31:18 -0300

    postgreesql

--- dao/db_config.py
+++ dao/db_config.py
@@ -1,7 +1,7 @@
 import sqlite3
-
+import psycopg2
 # path / url de conexão
-DB_PATH = "banco_escola.db"
+DB_PATH = "postgresql://neondb_owner:npg_zuEkb7vg6WHq@ep-wispy-voice-acoq5mgs-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require"
 
 def get_connection():
-    return sqlite3.connect(DB_PATH)
+    return psycopg2.connect(DB_PATH)
